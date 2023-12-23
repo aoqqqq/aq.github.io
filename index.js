c8bb5744968f18fdf05465eeaaaf1f44cbd6d6ee
aoqqqq <3329604386@qq.com>
2023-12-23 23:56:48 +0800

    test

---
+++ index.js
@@ -0,0 +1,45 @@
+let file1 = new FileReader();
+function fff() {
+    <div> 
+        <p>zheshiyig fuy</p>
+    </div>
+    return;
+}
+let person =
+{
+    jjj:1234,
+    vvv:
+    {
+        v:"newww",
+    },
+};
+
+let jsons =
+[  
+    {
+        id: 123,
+        age: 2,
+        class: "三年6班李子明同学",
+        obj1:
+        {
+            zhishang:"SB",
+            zhishangnumber:-250
+        },
+    },
+    {
+        id: 233,
+        age: 6,
+        class: "三年一班李子明同学",
+        obj1:
+        {
+            zhishang:"Your brain is a pile of shit",
+            zhishangnumber:-2222222222229999999999999999999999999999999999999999999999999
+        },
+    }
+]
+    
+
+//let jfile = new Blob([JSON.stringify(jsons)],{type:"text/plain;charset=utf-8"});
+
+
+console.log(JSON.stringify(jsons));
